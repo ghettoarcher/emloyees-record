e5e0aba704c93b42c1b4810ce2f00b7b132b3cb9
AdnanAlezari <adnan30062000@gmail.com>
2024-04-09 19:58:37 +0200

    first commit

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,15 @@
+import './app-info.css';
+
+const AppInfo = () => {
+      return(
+            <div className="app-info">
+                  <h1>Employee record</h1>
+                  <h2>Total number of employees:</h2>
+                  <h2>Bonus will get: </h2>
+
+            </div>
+      )
+}
+
+
+export default AppInfo;
